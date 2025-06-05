6d550d0a070504bab2d18e5b01667ae3866e6c50
DrSoftman <168275864+DrSoftman@users.noreply.github.com>
2025-06-05 22:40:01 +0200

    Create mjölnir-ui.h

---
+++ kernel/ui/mjölnir-ui.h
@@ -0,0 +1,12 @@
+#ifndef MJÖLNIR_UI_H
+#define MJÖLNIR_UI_H
+
+extern "C" {
+#include "system/font.h"
+#include "system/screen.h"
+}
+
+#include "../core/util.h"
+#include "../mem/mem.h"
+
+#endif // MJÖLNIR_UI_H
