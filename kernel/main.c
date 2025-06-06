c9897baa1a7c33b6f9c649155c1358e1ccff5801
DrSoftman <168275864+DrSoftman@users.noreply.github.com>
2025-06-06 14:03:12 +0200

    Update main.c

--- kernel/main.c
+++ kernel/main.c
@@ -12,12 +12,12 @@ void _main(u32 magic) {
     irq_init();
 
     screen_init();
+    int running = 1; // 1 = true ; 0 = false 
 
-    bool
-
-    while (1) {
+    while (running == 1) {
         draw_rectangle(50, 50, 100, 40, COLOR(7, 3, 1)); // Example color
         screen_swap(); // Swap buffers to display the rectangle
+        
     }
 }
 
