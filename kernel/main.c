24cd3dfb597f71473298cc671ebbc8803a8205a0
DrSoftman <168275864+DrSoftman@users.noreply.github.com>
2025-06-06 18:29:06 +0200

    Update main.c

--- kernel/main.c
+++ kernel/main.c
@@ -4,16 +4,15 @@
 #include "core/util.h"
 
 void _main(u32 magic) {
-    idt_init();
-    isr_init();
-    fpu_init(); // floating point unit.
-    irq_init();
+    idt_init();	
+    isr_init();	
+    fpu_init(); 
+    irq_init(); 
     /*
     imap_kernel();
     init_vmm();
     */
 	
-
     screen_init();
     bool quit = false;
     
@@ -21,8 +20,6 @@ void _main(u32 magic) {
         screen_clear(COLOR(0, 0, 7));
         screen_swap();
     }
-
-    
 
     // if we ever end up here, loop infinitly...
     for (;;) {}
