07469922861a7d411245510931172f5fc7a9d216
DrSoftman <168275864+DrSoftman@users.noreply.github.com>
2025-06-06 21:29:00 +0200

    Update fs.h

--- kernel/fs/fs.h
+++ kernel/fs/fs.h
@@ -38,54 +38,6 @@ void fs_init();
 //           FILE OPERATIONS
 //=======================================
 
-// File Management Functions
-int open(const char *pathname, int flags, mode_t mode);
-int creat(const char *pathname, mode_t mode);
-int close(int fd);
-ssize_t read(int fd, void *buf, size_t count);
-ssize_t write(int fd, const void *buf, size_t count);
-off_t lseek(int fd, off_t offset, int whence);
-int unlink(const char *pathname);
-int remove(const char *pathname);
-
-// Directory Operations
-int mkdir(const char *pathname, mode_t mode);
-int rmdir(const char *pathname);
-DIR *opendir(const char *name);
-struct dirent *readdir(DIR *dirp);
-int closedir(DIR *dirp);
-int chdir(const char *path);
-char *getcwd(char *buf, size_t size);
-
-// File Information and Attributes
-int stat(const char *pathname, struct stat *statbuf);
-int fstat(int fd, struct stat *statbuf);
-int lstat(const char *pathname, struct stat *statbuf);
-int chmod(const char *pathname, mode_t mode);
-int fchmod(int fd, mode_t mode);
-int chown(const char *pathname, uid_t owner, gid_t group);
-int fchown(int fd, uid_t owner, gid_t group);
-int utime(const char *filename, const struct utimbuf *times);
-int utimes(const char *filename, const struct timeval times[2]);
-
-// File System Operations
-int mount(const char *source, const char *target, const char *filesystemtype, unsigned long mountflags, const void *data);
-int umount(const char *target);
-void sync(void);
-int fsync(int fd);
-int truncate(const char *path, off_t length);
-int ftruncate(int fd, off_t length);
-
-// Advanced Operations
-int link(const char *oldpath, const char *newpath);
-int symlink(const char *target, const char *linkpath);
-ssize_t readlink(const char *pathname, char *buf, size_t bufsiz);
-int rename(const char *oldpath, const char *newpath);
-int dup(int oldfd);
-int dup2(int oldfd, int newfd);
-
-// I/O Control
-int ioctl(int fd, unsigned long request, ...);
-int fcntl(int fd, int cmd, ...);
+// TODO: Declare the functions for the file operations
 
 #endif // FS_H
