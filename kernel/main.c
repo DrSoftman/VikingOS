8a44a3a9a8aaaaa6cc12d1c3f3e60cfeb9339416
DrSoftman <168275864+DrSoftman@users.noreply.github.com>
2025-06-06 12:41:05 +0200

    Update main.c

--- kernel/main.c
+++ kernel/main.c
@@ -1,4 +1,26 @@
 #include "core/util.h"
+#include "core/interrupts.h"
+
+void quit() {
+    _asm("hlt");
+}
+
+void _main(u32 magic) {
+    idt_init();
+    isr_init();
+    fpu_init();
+    irq_init();
+
+    screen_init();
+
+    bool
+
+    while (1) {
+        draw_rectangle(50, 50, 100, 40, COLOR(7, 3, 1)); // Example color
+        screen_swap(); // Swap buffers to display the rectangle
+    }
+}
+
 void _main(u32 magic) {
   for (;;) {}
 }
