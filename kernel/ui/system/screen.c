cafb43749c1156ca334d07d9a416c7e7feeb8a04
DrSoftman <168275864+DrSoftman@users.noreply.github.com>
2025-06-06 11:25:15 +0200

    Update screen.c

--- kernel/ui/system/screen.c
+++ kernel/ui/system/screen.c
@@ -1,59 +1,41 @@
 #include "screen.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif 
+static u8 *BUFFER = (u8 *) 0xA0000;
 
-#define FB_ADDR 0xFD000000
-#define VIDEO_MODE 0x117 
-
-
-static u16* BUFFER = NULL;
-
-
-u8 *sbuffers[2];
+// double buffers
+u8 _sbuffers[2][SCREEN_SIZE];
 u8 _sback = 0;
 
-#define CURRENT ((u16*)sbuffers[_sback])
+#define CURRENT (_sbuffers[_sback])
 #define SWAP() (_sback = 1 - _sback)
 
+// VGA control port addresses
+#define PALETTE_MASK 0x3C6
+#define PALETTE_READ 0x3C7
+#define PALETTE_WRITE 0x3C8
+#define PALETTE_DATA 0x3C9
+
 void screen_swap() {
-    // SCREEN_SIZE is in bytes, divide by two.
-    memcpy(BUFFER, CURRENT, SCREEN_SIZE);
+    memcpy(BUFFER, &CURRENT, SCREEN_SIZE);
     SWAP();
 }
 
-void screen_clear(u16 color) {
-    u16 *current_buffer = CURRENT;
-    for (size_t i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) {
-        current_buffer[i] = color;
-    }
+void screen_clear(u8 color) {
+    memset(&CURRENT, color, SCREEN_SIZE);
 }
-
 
 void screen_init() {
-    if (!VIDEO_MODE) {
-        panic("ERROR: VARIABLE - VIDEO_MODE not found!");
+    // configure palette with 8-bit RRRGGGBB color
+    outportb(PALETTE_MASK, 0xFF);
+    outportb(PALETTE_WRITE, 0);
+    for (u8 i = 0; i < 255; i++) {
+        outportb(PALETTE_DATA, (((i >> 5) & 0x7) * (256 / 8)) / 4);
+        outportb(PALETTE_DATA, (((i >> 2) & 0x7) * (256 / 8)) / 4);
+        outportb(PALETTE_DATA, (((i >> 0) & 0x3) * (256 / 4)) / 4);
     }
-    
-    BUFFER = (u16 *)FB_ADDR;
-    
 
-    size_t buffer_size = SCREEN_SIZE; 
-    sbuffers[0] = (u8 *)malloc(buffer_size);
-    sbuffers[1] = (u8 *)malloc(buffer_size);
-    
-    if (!sbuffers[0] || !sbuffers[1]) {
-        panic("UNABLE TO ALLOCATE BUFFERS");
-    }
-    
-
-    memset(sbuffers[0], 0, buffer_size);
-    memset(sbuffers[1], 0, buffer_size);
+    // set color 255 = white
+    outportb(PALETTE_DATA, 0x3F);
+    outportb(PALETTE_DATA, 0x3F);
+    outportb(PALETTE_DATA, 0x3F);
 }
-
-
-
-#ifdef __cplusplus
-}
-#endif
