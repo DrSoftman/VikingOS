a7398949266f81b7ed894532774e5c09bb0fa26a
DrSoftman <168275864+DrSoftman@users.noreply.github.com>
2025-06-06 19:57:59 +0200

    Update mjölnir.h

---
+++ kernel/ui/mjölnir.h
@@ -0,0 +1,11 @@
+#ifndef MJÖLNIR_H
+#define MJÖLNIR_H
+
+#include "system/font.h"
+#include "system/screen.h"
+#include "../mem/mem.h"
+
+#define MAX_WINDOWS 16
+#define MAX_WIDGETS 1000
+
+#endif // MJÖLNIR_H
