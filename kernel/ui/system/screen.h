9ea328472ec2e3d8a0d93ddcb8ee2eba942c1fc6
DrSoftman <168275864+DrSoftman@users.noreply.github.com>
2025-06-06 11:25:37 +0200

    Update screen.h

--- kernel/ui/system/screen.h
+++ kernel/ui/system/screen.h
@@ -1,69 +1,53 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include "../../core/util.h"
-#include "../../core/error.h"
-#include "../../mem/mem.h"
 
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 200
+#define SCREEN_SIZE (SCREEN_WIDTH * SCREEN_HEIGHT)
 
-#define SCREEN_WIDTH 1024
-#define SCREEN_HEIGHT 768
-#define BPP 2  // 16-bit = 2 bytes per pixel
-#define SCREEN_SIZE (SCREEN_WIDTH * SCREEN_HEIGHT * BPP)
+#define COLOR(_r, _g, _b)((u8)( \
+    (((_r) & 0x7) << 5) |       \
+    (((_g) & 0x7) << 2) |       \
+    (((_b) & 0x3) << 0)))
 
+#define COLOR_R(_index) (((_index) >> 5) & 0x7)
+#define COLOR_G(_index) (((_index) >> 2) & 0x7)
+#define COLOR_B(_index) (((_index) >> 0) & 0x3)
 
-extern u8 *sbuffers[2];
+#define COLOR_ADD(_index, _d) __extension__({   \
+        __typeof__(_index) _c = (_index);       \
+        __typeof__(_d) __d = (_d);              \
+        COLOR(                                  \
+            CLAMP(COLOR_R(_c) + __d, 0, 7),     \
+            CLAMP(COLOR_G(_c) + __d, 0, 7),     \
+            CLAMP(COLOR_B(_c) + __d, 0, 3)      \
+        );})
+
+extern u8 _sbuffers[2][SCREEN_SIZE];
 extern u8 _sback;
 
+#define screen_buffer() (_sbuffers[_sback])
 
-#define screen_buffer() (sbuffers[_sback])
+#define screen_set(_p, _x, _y)\
+    (_sbuffers[_sback][((_y) * SCREEN_WIDTH + (_x))]=(_p))
 
-#define screen_set(_p, _x, _y) \
-    (((u16*)sbuffers[_sback])[((_y) * SCREEN_WIDTH + (_x))] = (_p))
+#define screen_offset(_x, _y) (screen_buffer()[(_y) * SCREEN_WIDTH + (_x)])
 
+#define screen_fill(_c, _x, _y, _w, _h) do {\
+        __typeof__(_x) __x = (_x);\
+        __typeof__(_y) __y = (_y);\
+        __typeof__(_w) __w = (_w);\
+        __typeof__(_y) __ymax = __y + (_h);\
+        __typeof__(_c) __c = (_c);\
+        for (; __y < __ymax; __y++) {\
+            memset(&screen_buffer()[__y * SCREEN_WIDTH + __x], __c, __w);\
+        }\
+    } while (0)
 
-#define screen_offset(_x, _y) \
-    (&((u16*)screen_buffer())[(_y) * SCREEN_WIDTH + (_x)])
-
-
-#define screen_fill(_c, _x, _y, _w, _h) do { \
-    int __x = (_x); \
-    int __y = (_y); \
-    int __w = (_w); \
-    int __h = (_h); \
-    u16 __c = (_c); \
-    u16 *buffer = (u16*)screen_buffer(); \
-    for (int row = 0; row < __h; row++) { \
-        for (int col = 0; col < __w; col++) { \
-            buffer[((__y + row) * SCREEN_WIDTH + (__x + col))] = __c; \
-        } \
-    } \
-} while (0)
-
-
-// Function declarations
 void screen_swap();
-void screen_clear(u16 color);  
+void screen_clear(u8 color);
 void screen_init();
-int setVideoMode(u16 mode);
-
-
-// red: 5 bits, green: 6 bits, blue: 5 bits
-#define COLOR(_r, _g, _b) \
-    ((u16)((((_r) & 0x1F) << 11) | (((_g) & 0x3F) << 5) | ((_b) & 0x1F)))
-
-
-#define COLOR_RGB888(_r, _g, _b) \
-    ((u16)(((((_r) >> 3) & 0x1F) << 11) | ((((_g) >> 2) & 0x3F) << 5) | (((_b) >> 3) & 0x1F)))
-
-
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
